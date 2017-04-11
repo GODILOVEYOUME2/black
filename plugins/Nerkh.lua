d576ddd6f19dc7087ff4c92300a45b6df832efa9
blacknewn <targetingsudo@gmail.com>
2017-04-11 12:11:48 +0430

    Create Nerkh.lua

---
+++ plugins/Nerkh.lua
@@ -0,0 +1,61 @@
+
+Skip to content
+This repository
+
+    Pull requests
+    Issues
+    Gist
+
+    @blacknewn
+
+0
+5
+
+    5
+
+mahdiroo/MaTaDoR
+Code
+Issues 0
+Pull requests 0
+Projects 0
+Wiki
+Pulse
+Graphs
+MaTaDoR/plugins/Nerkh.lua
+74cf6b2 on Feb 26
+@mahdiroo mahdiroo Add files via upload
+31 lines (29 sloc) 614 Bytes
+--Begin Nerkh.lua By @MahDiRoO
+do
+ function run(msg, matches)
+return [[ 
+تعرفه ﴿*MaTaDoT*﴾
+`1》ماهــــــــــیانـــــہ🌀`
+_5000✔_
+`2》سه ماهـــــــہ🌀`
+_14000✔_
+`3》برای هــــمیــشــــہ🌀`
+_25000✔_
+*☠پرداخت به دو صورت:*
+_1.💲کارت به کارت🎗_
+_2.💲شارژ با پرداخت بیشتر🎗_
+*〰〰〰〰〰〰〰〰*
+`برای خرید به ایدی:`
+@MahDiRoO
+]]
+end
+return {
+patterns = {
+"^[!/#][Nn]erkh$",
+"^[Nn]erkh$",
+"^k,خ$"
+},
+run = run
+}
+end
+--End Nerkh.lua--
+
+    Contact GitHub API Training Shop Blog About 
+
+    © 2017 GitHub, Inc. Terms Privacy Security Status Help 
+
