7441ea0634053fe8f303af4cb9c8950c7d2e632b
blacknewn <targetingsudo@gmail.com>
2017-04-11 10:53:00 +0430

    Update and rename matador.sh to beybal.sh

--- beybal.sh
+++ beybal.sh
@@ -67,7 +67,7 @@ if [ ! -f ./tg/tgcli ]; then
    echo -e "     >>>>                       We Are Not Alliance                             "
    echo -e "     >>>>                       We Are Family                                   "
    echo -e "     >>>>                       We Are The Best :-)                             "
-   echo -e "     >>>>                         @MahDiRoO                                     "
+   echo -e "     >>>>                         @GODILOVEYOUME2                                    "
    echo -e "\033[0;00m"
    echo -e "\e[36m"
    logo_play
