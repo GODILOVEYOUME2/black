d18f7abb8b204745ce69daa6c24f93cce9cba465
blacknewn <targetingsudo@gmail.com>
2017-04-11 11:37:28 +0430

    Update Tools.lua

--- plugins/Tools.lua
+++ plugins/Tools.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = 377450049 -- put Your ID here! <===
+local SUDO = 202024626 -- put Your ID here! <===
 function exi_files(cpath)
     local files = {}
     local pth = cpath
