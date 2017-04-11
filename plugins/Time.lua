3d66adba64a3a065a108ea3fe1821d39087888d0
blacknewn <targetingsudo@gmail.com>
2017-04-11 15:11:36 +0430

    Update Time.lua

--- plugins/Time.lua
+++ plugins/Time.lua
@@ -1,7 +1,7 @@
 --Begin Time.lua By @MahDiRoO
 function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
-if res ~= 200 then return "No connection" end
+if res ~= 200 then return "مشکل اتصال" end
 local jdat = json:decode(url)
 local text = '*Ir Time:* _'..jdat.FAtime..'_\n*Ir Data:* _'..jdat.FAdate..'_\n------------\n*En Time:* _'..jdat.ENtime..'_\n *En Data:* _'..jdat.ENdate.. '_\n'
   tdcli.sendMessage(msg.chat_id_, 0, 1, text, 1, 'md')
