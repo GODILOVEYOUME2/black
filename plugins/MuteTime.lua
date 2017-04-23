25171ebac430a5d02bb1fb8dfe1e52f3e51c8cac
samtaylor <samtaylor@tmails.net>
2017-04-23 09:28:29 +0430

    Update MuteTime.lua

--- plugins/MuteTime.lua
+++ plugins/MuteTime.lua
@@ -1,16 +1,16 @@
-﻿--Begin MuteTime.lua By @MahDiRoO
+
 local function pre_process(msg)
   local hash = 'mute_time:'..msg.chat_id_
-  if redis:get(hash) and gp_type(msg.chat_id_) == 'channel' and not is_mod(msg)  then
+  if redis:get(hash) and gp_type(msg.chat_id_) == 'channel' and not is_admin(msg)  then
     tdcli.deleteMessages(msg.chat_id_, {[0] = tonumber(msg.id_)})
   end
  end
  
 local function run(msg, matches)
-  if matches[1]:lower() == 'mt' and is_mod(msg) or matches[1]:lower() == 'بیصدا' and is_mod(msg) then
+  if matches[1]:lower() == 'mt' and is_admin(msg) then
      local hash = 'mute_time:'..msg.chat_id_
      if not matches[2] then
-		return "_لطفا ساعت و دقیقه را وارد نمایید!_"
+		return "_لطفا ساعت و دقیقه را وارد نمایید🕔!_"
   else
      local hour = string.gsub(matches[2], 'h', '')
      local num1 = tonumber(hour) * 3600
@@ -18,10 +18,10 @@ local function run(msg, matches)
      local num2 = tonumber(minutes) * 60
      local num4 = tonumber(num1 + num2)
 	 redis:setex(hash, num4, true)
-     return "⛔️گروه به مدت: \n`"..matches[2].."` ساعت\n`"..matches[3].."` دقیقه \nتعطیل میباشد.️"
+     return "⛔️گروه به مدت: \n`"..matches[2].."` 🕔ساعت\n`"..matches[3].."` 🕔دقیقه \nتعطیل میباشد.️"
     end
   end
-  if matches[1]:lower() == 'unmt' and is_mod(msg) or matches[1]:lower() == 'باصدا' and is_mod(msg) then
+  if matches[1]:lower() == 'unmt' and is_admin(msg) then
      local hash = 'mute_time:'..msg.chat_id_
      redis:del(hash)
      return "*✅گروه برای ارسال پیام کاربران باز شد.*"
@@ -32,14 +32,8 @@ return {
       '^[/!#]([Mm][Tt])$',
       '^[/!#]([Uu][Nn][Mm][Tt])$',
 	  '^[/!#]([Mm][Tt]) (%d+) (%d+)$',
-      '^([Mm][Tt])$',
-      '^([Uu][Nn][Mm][Tt])$',
-	  '^([Mm][Tt]) (%d+) (%d+)$',
-	  '^(بیصدا)$',
-      '^(باصدا)$',
-	  '^(بیصدا) (%d+) (%d+)$',
  },
   run = run,
   pre_process = pre_process
 }
---End MuteTime.lua--
+--end by #@GODILOVEYOUME2#
