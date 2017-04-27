4280cbd0faf03027b9516ce1027c7fc8b3fa3fef
samtaylor <samtaylor@tmails.net>
2017-04-27 16:16:10 +0430

    Update Tools.lua

--- plugins/Tools.lua
+++ plugins/Tools.lua
@@ -1158,7 +1158,7 @@ return chat_list(msg)
 	   tdcli.sendMessage(matches[2], 0, 1, "Group has been removed by admin command", 1, 'html')
     return '_Group_ *'..matches[2]..'* _removed_'
 		end
-if matches[1] == 'matador' or matches[1] == 'Matador' then
+if matches[1] == 'Black' or matches[1] == 'Black' then
 return tdcli.sendMessage(msg.to.id, msg.id, 1, _config.info_text, 1, 'html')
     end
 if matches[1] == 'adminlist' and is_admin(msg) or matches[1] == 'Adminlist' and is_admin(msg) then
@@ -1501,4 +1501,4 @@ patterns = {
 }, 
 run = run, pre_process = pre_process
 }
--- #End By @MahDiRoO
+-- #End By @GODILOVEYOUME2
